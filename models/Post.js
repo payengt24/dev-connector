65dac2e611dba0e7f71d36b34373c33e9ebd27ef
Pa Yeng Thao <payengt24@gmail.com>
2019-02-24 14:14:57 -0600

    update model schema

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const moongoose = require('mongoose');
+const Schema = moongoose.Schema;
+
+//Create Schema
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+ 
+    likes: [{
+        user: Schema.Types.ObjectId,
+        ref: 'users'
+
+    }],
+    comments: [{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Post = moongoose.model('post', PostSchema)
